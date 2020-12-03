2bda71e411b85d85f9255aa8a5da74662703df83
DimitriNakos <dimi1nakos@gmail.com>
2020-12-02 21:28:30 -0500

    cleaned up the code and tested before finishing up.

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 const employeeArr = []
+
+//  Renders out the HTML and write's the file
 const renderTeam = () => {
     if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR)
@@ -21,10 +21,10 @@ const renderTeam = () => {
         } else {
             console.log("... writing file")
         }
-
     })
 }
 
+//  Uses Inquirer to get the correct parameters for the Engineer
 const makeEngineer = () => {
     inquirer.prompt([
         {
@@ -54,6 +54,7 @@ const makeEngineer = () => {
     })
 }
 
+//  Uses Inquirer to get the correct parameters for the Intern
 const makeIntern = () => {
     inquirer.prompt([
         {
@@ -83,6 +84,7 @@ const makeIntern = () => {
     })
 }
 
+//  Uses Inquirer to see what to do after you create an Employee
 const whatNext = () => {
     inquirer.prompt([
         {
@@ -107,8 +109,8 @@ const whatNext = () => {
     })
 }
 
+//  The init function
 function init() {
-
     inquirer.prompt([
         {
             type: 'input',
@@ -137,29 +139,4 @@ function init() {
     })
 }
 
-
-
 init()
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
